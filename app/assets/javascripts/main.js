d8ab76bd401c9b46aece683c7d6a48a95c3dd1b8
evgeny.zibrov <evgeny.zibrov@gmail.com>
2012-10-02 18:15:59 +0300

    Add comments to posts

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,4 +3,11 @@ $(function(){
 	$('.editor').kendoEditor({
      encoded: false
  	});
+ 	$('.show_comment_form').live('click',function(){
+ 		var elem = $(this).closest('.comment_block.span12').find('.add_comment');
+ 		if(elem.height() == 0)
+ 			elem.animate({height: '0px'},500);
+ 		else
+ 			elem.animate({height: '400px'}, 500);
+ 	});
 });
